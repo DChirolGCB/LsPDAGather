9e280d2e0761536d77f87d422602ab519e3f566f
unknown <davidchirolpro@gmail.com>
2021-10-25 04:20:45 +0200

    SAMARCH

--- func.py
+++ func.py
@@ -1,11 +1,57 @@
+# ID CLIENT pgjgwxpokkqeg4sp1jcxffyeawaepc
+# TOKEN jwuxby25t6avtfmst0ko7ppfyevtyx
+# secret = h9xkw35o2vgi0jvfoku2sf1x3fk25o
+from datetime import datetime as dateuser
+import datetime
+import warnings
+import sys
+import copy
+import string
+from pyparsing import Word, alphas
+import csv
+import datetime
+import os.path
 import csv
 import time
 from openpyxl import Workbook
 import gspread
+from openpyxl.utils import get_column_letter
+import openpyxl
+from numpy import reshape
+import bar_chart_race as bcr
+from openpyxl.workbook import Workbook
+from openpyxl import load_workbook
+import pandas as pd
+import csv
 # import matplotlib.pyplot as plt
 # import pandas as pd
 import requests
 from oauth2client.service_account import ServiceAccountCredentials
+import re
+import time
+import unittest
+from tempfile import NamedTemporaryFile
+import gspread
+import requests
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.cell import Cell
+import bar_chart_race as bcr
+from openpyxl.descriptors import (
+    String,
+    Sequence,
+    Integer,
+)
+from openpyxl.descriptors.serialisable import Serialisable
+from openpyxl.reader.excel import load_workbook
+from openpyxl.styles import Color, Fill
+from openpyxl.styles import numbers
+from openpyxl.utils import quote_sheetname
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.workbook import Workbook
+from openpyxl.worksheet.datavalidation import DataValidation
+import matplotlib.pyplot as plt
+plt.rcParams['animation.ffmpeg_path'] = "C:/FFmpeg/bin/ffmpeg"
 
 
 def Twitch_Auth():
@@ -13,7 +59,7 @@ def Twitch_Auth():
              "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
 
     credentials = ServiceAccountCredentials.from_json_keyfile_name(
-        'secrets.json', scope)
+        'F:\ProjetsPython\StreamStatFinalM\secrets.json', scope)
     client = gspread.authorize(credentials)
     client_id = 'pgjgwxpokkqeg4sp1jcxffyeawaepc'
     client_secret = 'h9xkw35o2vgi0jvfoku2sf1x3fk25o'
@@ -25,27 +71,76 @@ def Twitch_Auth():
         "grant_type": 'client_credentials'
     }
     r = requests.post('https://id.twitch.tv/oauth2/token', body)
-    print(r)
     keys = r.json()
     headers = {
         'Client-ID': client_id,
         'Authorization': 'Bearer ' + keys['access_token']
     }
     header = ['streamer_login', 'viewer_count', 'PDA', 'title']
-    stream = requests.get('https://api.twitch.tv/helix/streams?first=100&language=fr', headers=headers)
-    print(header)
+    stream = requests.get(
+        'https://api.twitch.tv/helix/streams?first=100&language=fr', headers=headers)
     return stream
 
 
+def split(word):
+    return [char for char in word]
+
+
+def col2num(col):
+    num = 0
+    for c in col:
+        if c in string.ascii_letters:
+            num = num * 26 + (ord(c.upper()) - ord('A')) + 1
+    return num
+
+
 def total_fr(stream_data):
-    print(stream_data)
+    (stream_data)
     total_fr = 0
     i = 0
     pda = 0
     totalpda = 0
-    while i < 99:
+    while i < 98:
         total_fr += stream_data['data'][i]['viewer_count']
         i += 1
 
-    print(f"Il y a {total_fr} viewers fr")
+    now = dateuser.now()
+    today = now.strftime("%H:%M:%S")
+    print(f"{today} : Il y a {total_fr} viewers fr sur les 100 premiers streams fr")
     return total_fr
+
+
+def getline(username, ws):
+    for columns in ws.iter_cols(1):
+        for cell in columns:
+            if cell.value == username:
+                return(cell.column)
+    return get_column_letter(len(ws['1']) + 1)
+
+
+def barchartrace(source_race):
+    df = pd.DataFrame(pd.read_excel(source_race))
+    bcr.bar_chart_race(
+        df=df,
+        filename='testvideo.mp4',
+        orientation='h',
+        sort='desc',
+        n_bars=20,
+        fixed_order=False,
+        fixed_max=True,
+        steps_per_period=20,
+        period_length=1000,
+        interpolate_period=False,
+        period_label={'x': .98, 'y': .3, 'ha': 'right', 'va': 'center'},
+        period_summary_func=lambda v, r: {'x': .98, 'y': .2,
+                                          's': f'Total deaths: {v.sum():,.0f}',
+                                          'ha': 'right', 'size': 11},
+        perpendicular_bar_func='median',
+        title='PDA jour du',
+        bar_size=.95,
+        shared_fontdict=None,
+        scale='linear',
+        fig=None,
+        writer=None,
+        bar_kwargs={'alpha': .7},
+        filter_column_colors=False)
--- main.py
+++ main.py
@@ -1,27 +1,68 @@
 # ID CLIENT pgjgwxpokkqeg4sp1jcxffyeawaepc
 # TOKEN jwuxby25t6avtfmst0ko7ppfyevtyx
 # secret = h9xkw35o2vgi0jvfoku2sf1x3fk25o
-print("test")
+from pyparsing import Word, alphas
+from func import total_fr
+from func import Twitch_Auth
+from func import getline
+from func import barchartrace
+from func import col2num
+from func import split
+#from webscrap import get_totalviewers
+import openpyxl
 import csv
 import datetime
 import os.path
-import re
+import csv
 import time
-import unittest
-from tempfile import NamedTemporaryFile
-
+from openpyxl.chart import BarChart, Series, Reference
 import gspread
+
+from openpyxl import workbook
+from numpy import reshape
+import bar_chart_race as bcr
+from openpyxl.workbook import Workbook
+from openpyxl import load_workbook
+import pandas as pd
+import csv
 # import matplotlib.pyplot as plt
 # import pandas as pd
 import requests
-# import win32com.client
-# import xlwings as xw
-from oauth2client.service_account import ServiceAccountCredentials
+import re
+import time
+from datetime import datetime
+
+import unittest
+from tempfile import NamedTemporaryFile
+import gspread
+import requests
 from openpyxl import Workbook
-from openpyxl import Workbook
-from openpyxl import load_workbook
 from openpyxl import load_workbook
 from openpyxl.cell import Cell
+import bar_chart_race as bcr
+from openpyxl.descriptors import (
+    String,
+    Sequence,
+    Integer,
+)
+import string
+import copy
+import sys
+import warnings
+import re
+import requests
+from oauth2client.service_account import ServiceAccountCredentials
+import re
+import time
+import unittest
+from lxml import html
+from tempfile import NamedTemporaryFile
+import gspread
+import requests
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from openpyxl.cell import Cell
+import bar_chart_race as bcr
 from openpyxl.descriptors import (
     String,
     Sequence,
@@ -31,58 +72,114 @@ from openpyxl.descriptors.serialisable import Serialisable
 from openpyxl.reader.excel import load_workbook
 from openpyxl.styles import Color, Fill
 from openpyxl.styles import numbers
+from openpyxl.utils import quote_sheetname
 from openpyxl.utils.dataframe import dataframe_to_rows
 from openpyxl.workbook import Workbook
+from openpyxl.worksheet.datavalidation import DataValidation
+from openpyxl.descriptors.serialisable import Serialisable
+from openpyxl.reader.excel import load_workbook
+from openpyxl.styles import Color, Fill
+from openpyxl.styles import numbers
+from openpyxl.utils import quote_sheetname
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.utils import get_column_letter
+from openpyxl.workbook import Workbook
+from openpyxl.worksheet.datavalidation import DataValidation
+from openpyxl.utils import get_column_letter
+from datetime import date
+import matplotlib.pyplot as plt
 
-from func import Twitch_Auth
-from func import total_fr
+
+# get_totalviewers()
+# exit()
+plt.rcParams['animation.ffmpeg_path'] = "C:/FFmpeg/bin/ffmpeg"
+
+today = date.today()
+try:
+    if not os.path.exists('XLfiles'):
+        os.makedirs('XLfiles')
+except OSError:
+    print('Error: Creating directory. ' + 'XLfiles')
+
+filepath = f"StatTwitch{today}"
+cfilepath = f"StatTwitch{today}.xlsx"
+print(filepath)
+# import matplotlib.pyplot as plt
+# import pandas as pd
+# import win32com.client
+# import xlwings as xw
+
 
 # Création excel
-
-wb = load_workbook(filename='test.xlsx')
+iter_path = 1
+while os.path.isfile(cfilepath):
+    cfilepath = filepath + '_0' + str(iter_path) + '.xlsx'
+    iter_path += 1
+os.chdir('XLfiles')
+wb = openpyxl.Workbook()
 wb.guess_types = True
 ws = wb.active
-# Connexion Twitch
+wb.save(filename=cfilepath)
 
-
-# Calcul Total Viewer Fr
-
-
-# Calcul PDA Top100
 
 totalpda = float(0)
 n = 1
 i = 0
+t = 'B'
+col = 1
+firstpassage = True
+line = 1
+# if n == 1:
+#    exit()
+# barchartrace('sample.xlsx')
 
 while n < 50:
+    totalviewers = 0
+
     stream = Twitch_Auth()
     stream_data = stream.json()
     stats_stream_fr = 0
     stats_stream_fr = total_fr(stream_data)
     total_frbis = stats_stream_fr
+    now = datetime.now()
+    today = now.strftime("%H:%M:%S")
     i = 0
     totalpda = float(0)
-    while i <= 9:
+    while i <= 15:
+
         pda = (stream_data['data'][i]['viewer_count'] * 100) / total_frbis
         pda = "{: .2f}".format(pda)
         username = stream_data['data'][i]['user_name']
         viewer_count = stream_data['data'][i]['viewer_count']
         title = stream_data['data'][i]['title']
         data = [username, pda]
-        ws[f'B{i+1+n}'] = username
-        ws[f'C{i+1+n}'] = float(pda)
-        ws[f'D{i+1+n}'] = viewer_count
-        ws[f'E{i+1+n}'] = title
+        if firstpassage:
+            ws[f'A{2}'] = today
+            ws[f'{t}1'] = username
+            ws[f'{t}2'] = float(pda)
+        else:
+            try:
+                t = get_column_letter(getline(username, ws))
+            except:
+                t = getline(username, ws)
+            ws[f'A{line}'] = today
+            ws[f"{t}1"] = username
+            ws[f"{t}{line}"] = float(pda)
         totalpda = "{: .2f}".format(totalpda)
         totalpda = float(totalpda) + float(pda)
+        t = col2num(t)
+        t = get_column_letter(t + 1)
         i += 1
+    line += 1
     wb.save("sample.xlsx")
-    time.sleep(15)
-    n += 11
+    firstpassage = False
+    wb.save(cfilepath)
+    time.sleep(2)
+# print(totalpda)
 
-print(totalpda)
 
 totalpda = "{: .2f}".format(totalpda)
+print(
+    f"Les 20 premiers streamers fr représentent {totalpda}% des vues du top 100")
 
-print(f"Les 100 premiers streamers fr représentent {totalpda}% des vues")
-
+wb.save(cfilepath)
